4704e7dd1f650d8e029a8de56dac13e7b0f096df
ekoniuh <dr_zek@mail.ru>
2018-09-17 21:53:30 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let del1 = equation.indexOf (" * x^2 ");
+  let a = +equation.substring(0,del1);
+  
+  console.log (a);
+
+  let del2 = del1 + " * x^2 ".length;
+  let del3 =  equation.indexOf (" * x ");
+  let b = +equation.substring(del2,del3).split('').filter(letter => letter != ' ').join('');
+  
+  console.log (b);
+
+  let c = +equation.substring(del3 + 5, equation.length).split('').filter(letter => letter != ' ').join('');
+ 
+  console.log (c);
+
+  let d = b*b - 4*a*c;
+  let x1 = Math.round(( - b - Math.sqrt (d) ) / (2*a));
+  let x2 = Math.round(( - b + Math.sqrt (d) ) / (2*a));
+  return [x1, x2].sort((a, b) => x1 - x2);
 }
+
